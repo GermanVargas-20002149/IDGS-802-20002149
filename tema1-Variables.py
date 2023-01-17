471d7d8ec3cdae69971b0fe784f907baf9b7736c
vargashernandezluisgerman <vargashernandezluisgerman@gmail.com>
2023-01-16 18:44:40 -0600

    Subo los primeros cinco temas de python

---
+++ tema1-Variables.py
@@ -0,0 +1,27 @@
+
+#Nombre de Variables
+
+
+'''
+* Tiene que que tener nombres significativos.
+* Relacionados con los que representa la variable.
+* NO deben ser Keywords (palabras reservadas)
+'''
+
+numero1 = "23"
+numero2 = "10"
+nombre = "German"
+correo = "gercool92@gmail.com"
+
+print(numero1 + numero2)
+
+#Conversion forzosas entre tipos de datos
+print("La suma es:")
+print(int(numero1) + int(numero2))
+
+sueldo = "1720.203"
+valorDecima = float(sueldo)
+print(valorDecima)
+
+edad = 110
+print(len(str(edad)))
